d617b31dd179a26109ae57acc1a78307d61b3be5
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2019-01-11 20:36:58 -0500

    Add cart manager
    Add business logic for cart routes

---
+++ src/managers/cart.js
@@ -0,0 +1,69 @@
+const Cart = require('../models/cart');
+const mongoose = require('mongoose');
+const route = '/cart/';
+let CartService = require('../services/cart');
+
+class CartManager {
+  constructor() {
+    this.cartService = new CartService();
+  }
+  async create() {
+    const newCart = new Cart({
+      _id: new mongoose.Types.ObjectId(),
+      products: [],
+      subtotalCost: 0,
+      timeCreated: new Date()
+    });
+    try {
+      const result = await this.cartService.create(newCart);
+      return {
+        status: 201,
+        json: {
+          message: "Cart added to database",
+          request: {
+            type: "POST",
+            url: `http://localhost:3000${route}`,
+          },
+          result,
+        },
+      };
+    } catch (error) {
+      return { status: 500, json: error };
+    }
+  }
+
+  // Find cart by id
+  async findById(query) {
+    // TODO: access cart only if authorized with JWT
+    const { id } = query;
+
+    try {
+      const result = await this.cartService.findById(id);
+      if (!result) {
+        return {
+          status: 404,
+          json: {
+            message: "Cart not found",
+          },
+        };
+      }
+
+      return {
+        status: 200,
+        json: {
+          message: "Cart pulled from database",
+          request: {
+            type: "GET",
+            url: `http://localhost:3000${route}/${id}`,
+          },
+          result,
+        },
+      };
+    } catch (error) {
+      console.log(error);
+      return { status: 500, json: error };
+    }
+  }
+}
+
+module.exports = CartManager;

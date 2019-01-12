dae8e650b2a212e97929250b3408ae6e31f8c09c
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2019-01-12 00:44:15 -0500

    Refactor product routes

--- src/routes/product.js
+++ src/routes/product.js
@@ -3,20 +3,20 @@ const router = express.Router();
 let ProductManager = require('../managers/product');
 const productManager = new ProductManager();
 
-router.post('/', async (req, res) => {
+router.post('/new', async (req, res) => {
   const result = await productManager.create(req.body);
-  const { status, json } = result;
-  res.status(status).json(json);
-});
-
-router.get('/id/:id', async (req, res) => {
-  const result = await productManager.findById(req.params.id);
   const { status, json } = result;
   res.status(status).json(json);
 });
 
 router.get('/all', async (req, res) => {
   const result = await productManager.find(req.query);
+  const { status, json } = result;
+  res.status(status).json(json);
+});
+
+router.get('/:id', async (req, res) => {
+  const result = await productManager.findById(req.params.id);
   const { status, json } = result;
   res.status(status).json(json);
 });

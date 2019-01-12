2a24be0bba975c47b67ed9f169f7f1c776c21032
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2019-01-11 22:56:51 -0500

    Update cart routes
    Change name of create cart route and remove cart patch route

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -4,20 +4,14 @@ let CartManager = require('../managers/cart');
 
 const cartManager = new CartManager();
 
-router.post("/", async (req, res) => {
+router.post("/new", async (req, res) => {
   const result = await cartManager.create();
   const { status, json } = result;
   res.status(status).json(json);
 });
 
-router.get("/id", async (req, res) => {
-  const result = await cartManager.find(req.query);
-  const { status, json } = result;
-  res.status(status).json(json);
-});
-
-router.patch("/", async (req, res) => {
-  const result = await cartManager.update(req.body);
+router.get("/", async (req, res) => {
+  const result = await cartManager.findById(req.query);
   const { status, json } = result;
   res.status(status).json(json);
 });
